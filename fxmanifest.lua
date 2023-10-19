9b0faee1b523ca82bc100ef3e51a171e152396d1
Peurost <dukuliukas@gmail.com>
2023-10-19 22:13:20 +0300

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'PEUREN DEVELOPMENT - peuren.tebex.io'
+lua54 'yes'
+
+shared_files {
+    'shared/*'
+}
+
+server_files {
+    'server/*'
+}
+
+client_files {
+    'server/*'
+}
---
+++ shared/config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.Framework = "QB" --ESX/QB - The framework your server is based on
+Config.SQLConnector = "oxmysql" --oxmysql/mysql-async - The SQL connector resource that you're using.
+
+Config.Inventory = "" -- ox_inventory - Leave empty if you use the default framework inventory.
+
+Config.Target = "qb-target" -- qb-target/ox_target - The target system that you're using.
