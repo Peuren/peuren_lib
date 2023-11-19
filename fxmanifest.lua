5649d59c7fa69341dcf4b0efb7f81e9b9dad8e9b
Kristijonas B <rencikas.business@gmail.com>
2023-11-19 14:05:27 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,6 +7,10 @@ lua54 'yes'
 
 client_script 'client/main.lua'
 server_script 'server/main.lua'
-shared_scripts { --[['@ox_lib/init.lua',]] 'utils/**/*.lua', 'shared/**/*.lua' }
+shared_scripts { 
+  --'@ox_lib/init.lua', --Remove the two -- if you're using ox_lib in your servber
+  'utils/**/*.lua', 
+  'shared/**/*.lua' 
+}
 
 files { 'client/**/*.lua' }
