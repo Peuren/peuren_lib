ab55958fb00db40ad94ce27941bb5f9212f28bfb
Kristijonas B <rencikas.business@gmail.com>
2023-11-19 14:04:27 +0200

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -14,15 +14,15 @@ Config.InventoryResource = 'ox_inventory' -- The inventory system folder name th
 
 Config.Target = "ox_target" -- qb-target / ox_target / qtarget - The target system that you're using.
 
-Config.ProgessBar = 'ox_lib' -- ox_lib / qb - The progress bar system that you're using.
+Config.ProgessBar = 'ox_lib' -- ox_lib (Uncomment ox_lib export in fxmanifest.lua file) / qb - The progress bar system that you're using.
 
-Config.Menu = 'ox_lib' -- ox_lib / qb / nh-context / esx_menu_default / esx_context - The menu system that you're using.
+Config.Menu = 'ox_lib' -- ox_lib (Uncomment ox_lib export in fxmanifest.lua file) / qb / nh-context / esx_menu_default / esx_context - The menu system that you're using.
 Config.MenuResource = 'ox_lib'
 
-Config.Input = 'ox_lib' -- ox_lib / qb-input / nh-keyboard - The input system that you're using
+Config.Input = 'ox_lib' -- ox_lib (Uncomment ox_lib export in fxmanifest.lua file) / qb-input / nh-keyboard - The input system that you're using
 Config.InputResource = 'ox_lib' -- The input system folder name that you're using
 
-Config.Notifications = "ox_lib" -- ox_lib / qb / esx / okok / mythic - The notification system that your're using
+Config.Notifications = "ox_lib" -- ox_lib (Uncomment ox_lib export in fxmanifest.lua file) / qb / esx / okok / mythic - The notification system that your're using
 Config.NotificationsResource = "ox_lib" -- The notification system folder name that you're using
 
 Config.WeatherSync = 'qb-weathersync'-- qb-weathersync / cd_easytime - The weathersync system thet you're using
