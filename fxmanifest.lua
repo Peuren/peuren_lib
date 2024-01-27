6882c30d0a9e1554e9681f16c3c22765924561bd
Renčikas <rencikas.business@gmail.com>
2024-01-27 17:07:17 +0200

    Added polyzone component

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,7 +8,14 @@ lua54 'yes'
 client_scripts { 'client/groups/**',  "client/inventory/carryItems.lua", 'client/main.lua', }
 server_scripts { 'server/groups/**', 'server/main.lua' }
 shared_scripts { 
-  '@ox_lib/init.lua', --Remove the two -- if you're using ox_lib in your server
+  -- '@PolyZone/client.lua', --Remove the two -- if you're using polyzone in your server
+	-- '@PolyZone/BoxZone.lua', --Remove the two -- if you're using polyzone in your server
+	-- '@PolyZone/EntityZone.lua', --Remove the two -- if you're using polyzone in your server
+	-- '@PolyZone/CircleZone.lua', --Remove the two -- if you're using polyzone in your server
+	-- '@PolyZone/ComboZone.lua', --Remove the two -- if you're using polyzone in your server
+
+  -- '@ox_lib/init.lua', --Remove the two -- if you're using ox_lib in your server
+  
   'utils/**/*.lua', 
   'shared/**/*.lua' 
 }
