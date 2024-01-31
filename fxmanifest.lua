2e9f20c8186474b853d67fbe6dba3e21ef4525ef
Peurost <dukuliukas@gmail.com>
2024-01-31 19:09:46 +0200

    fxManifest fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,14 +5,14 @@ author 'PEUREN DEVELOPMENT - peuren.tebex.io'
 version '0.0.1'
 lua54 'yes'
 
-client_scripts { 'client/groups/**', "client/inventory/carryItems.lua", 'client/main.lua', "client/menu/skillMenu.lua"}
+client_scripts { 'client/groups/**', 'client/main.lua', "client/menu/skillMenu.lua", "client/inventory/carryItems.lua"}
 server_scripts { 'server/groups/**', 'server/main.lua' }
 shared_scripts { 
-  '@PolyZone/client.lua', --Remove the two -- if you're using polyzone in your server
-  '@PolyZone/BoxZone.lua', --Remove the two -- if you're using polyzone in your server
-  '@PolyZone/EntityZone.lua', --Remove the two -- if you're using polyzone in your server
-  '@PolyZone/CircleZone.lua', --Remove the two -- if you're using polyzone in your server
-  '@PolyZone/ComboZone.lua', --Remove the two -- if you're using polyzone in your server
+  -- '@PolyZone/client.lua', --Remove the two -- if you're using polyzone in your server
+  -- '@PolyZone/BoxZone.lua', --Remove the two -- if you're using polyzone in your server
+  -- '@PolyZone/EntityZone.lua', --Remove the two -- if you're using polyzone in your server
+  -- '@PolyZone/CircleZone.lua', --Remove the two -- if you're using polyzone in your server
+  -- '@PolyZone/ComboZone.lua', --Remove the two -- if you're using polyzone in your server
 
   -- '@ox_lib/init.lua', --Remove the two -- if you're using ox_lib in your server
   
