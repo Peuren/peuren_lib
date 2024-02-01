68b5bd82aea40e158eef705eca5a101d9d3bff7a
Renčikas <rencikas.business@gmail.com>
2024-02-01 08:50:27 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ author 'PEUREN DEVELOPMENT - peuren.tebex.io'
 version '0.0.1'
 lua54 'yes'
 
-client_scripts { 'client/groups/**', 'client/main.lua', "client/menu/skillMenu.lua", "client/inventory/carryItems.lua"}
+client_scripts { 'client/groups/**', "client/inventory/carryItems.lua", 'client/main.lua', "client/menu/skillMenu.lua" }
 server_scripts { 'server/groups/**', 'server/main.lua' }
 shared_scripts { 
   -- '@PolyZone/client.lua', --Remove the two -- if you're using polyzone in your server
@@ -14,7 +14,7 @@ shared_scripts {
   -- '@PolyZone/CircleZone.lua', --Remove the two -- if you're using polyzone in your server
   -- '@PolyZone/ComboZone.lua', --Remove the two -- if you're using polyzone in your server
 
-  -- '@ox_lib/init.lua', --Remove the two -- if you're using ox_lib in your server
+  --'@ox_lib/init.lua', --Remove the two -- if you're using ox_lib in your server
   
   'utils/**/*.lua', 
   'shared/**/*.lua' 
