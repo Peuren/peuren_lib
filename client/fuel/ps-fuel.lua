d0122dab15cefce3c23cdf05e20a6e72faee1d1c
Renčikas <rencikas.business@gmail.com>
2024-02-09 07:57:05 +0200

    ps-fuel export fix

--- client/fuel/ps-fuel.lua
+++ client/fuel/ps-fuel.lua
@@ -1,11 +1,11 @@
 Fuel = {
     GetFuel = function(veh, plate)
         if not veh or not plate then return 0 end
-        return exports[Config.FuelResource]:getFuel(veh)
+        return exports[Config.FuelResource]:GetFuel(veh)
     end,
 
     SetFuel = function(veh, plate, level)
-        exports[Config.FuelResource]:setFuel(veh, level, 'RON91')
+        exports[Config.FuelResource]:SetFuel(veh, level, 'RON91')
     end
 }
 
--- shared/config.lua
+++ shared/config.lua
@@ -4,16 +4,16 @@ Config.Debug = true -- Adds additional prints and enables all other resource deb
 Config.Language = 'en' -- Language file name, this means, that it will only load files from script locales folders that contain this name, 
                        -- if this is an invalid locale name on a resource it will default to english
 
-Config.Framework = "qb" -- esx / qb - The framework your server is based on
-Config.FrameworkResource = "qb-core" -- Framework resource name
+Config.Framework = "esx" -- esx / qb - The framework your server is based on
+Config.FrameworkResource = "es_extended" -- Framework resource name
 
 Config.SQLConnector = "oxmysql" -- oxmysql / mysql-async  / ghmattimysql - The SQL connector resource that you're using.
 
 Config.Inventory = "ox_inventory" -- esx_inventory / qb-inventory / ox_inventory / qs-inventory - The inventory system that you're using
 Config.InventoryResource = 'ox_inventory' -- The inventory system folder name that you're using
 
-Config.Target = "qb-target" -- qb-target / ox_target / qtarget - The target system that you're using.
-Config.TargetResource = 'qb-target' -- The inventory system folder name that you're using
+Config.Target = "ox_target" -- qb-target / ox_target / qtarget - The target system that you're using.
+Config.TargetResource = 'ox_target' -- The inventory system folder name that you're using
 
 Config.ProgessBar = 'ox_lib' -- ox_lib / qb - The progress bar system that you're using.
 
