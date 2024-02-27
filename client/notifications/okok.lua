95cc7824fa2d805b44f24ac4ef6219fed0ea87d7
Renčikas <rencikas.business@gmail.com>
2024-02-27 19:50:28 +0200

    okok notification fix

--- client/notifications/okok.lua
+++ client/notifications/okok.lua
@@ -1,4 +1,6 @@
 function Notify(title, desc, type)
+    if not desc then desc = "" end 
+    if not title then title = "" end
     exports[Config.NotificationsResource]:Alert(title, desc, 5000, type, false)
 end
 
