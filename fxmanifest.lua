8d2486035e8b77e7c569834b593ada1a3c8def98
Renčikas <rencikas.business@gmail.com>
2024-04-21 10:02:59 +0300

    fix: qbx module loading sequence

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,10 +3,13 @@ game 'gta5'
 
 author 'PEUREN DEVELOPMENT - peuren.tebex.io'
 version '0.1.0'
+
 lua54 'yes'
+use_experimental_fxv2_oal 'yes'
 
 client_scripts { 
   -- '@ox_core/imports/client.lua', --Remove the two -- if you're using ox_core in your server
+
   'client/groups/**', 
   "client/inventory/carryItems.lua", 
   'client/main.lua', 
@@ -14,14 +17,14 @@ client_scripts {
 }
 
 shared_scripts { 
-  '@PolyZone/client.lua', --Remove the two -- if you're using polyzone in your server
-  '@PolyZone/BoxZone.lua', --Remove the two -- if you're using polyzone in your server
-  '@PolyZone/EntityZone.lua', --Remove the two -- if you're using polyzone in your server
-  '@PolyZone/CircleZone.lua', --Remove the two -- if you're using polyzone in your server
-  '@PolyZone/ComboZone.lua', --Remove the two -- if you're using polyzone in your server
+  -- '@PolyZone/client.lua', --Remove the two -- if you're using polyzone in your server
+  -- '@PolyZone/BoxZone.lua', --Remove the two -- if you're using polyzone in your server
+  -- '@PolyZone/EntityZone.lua', --Remove the two -- if you're using polyzone in your server
+  -- '@PolyZone/CircleZone.lua', --Remove the two -- if you're using polyzone in your server
+  -- '@PolyZone/ComboZone.lua', --Remove the two -- if you're using polyzone in your server
 
-  --'@qbx_core/modules/lib.lua', --Remove the two -- if you're using qbx_core in your server
-  --'@ox_lib/init.lua', --Remove the two -- if you're using ox_lib in your server
+  -- '@ox_lib/init.lua', --Remove the two -- if you're using ox_lib in your server
+  -- '@qbx_core/modules/lib.lua', --Remove the two -- if you're using qbx_core in your server
   
   'utils/**/*.lua', 
   'shared/**/*.lua' 
@@ -29,9 +32,9 @@ shared_scripts {
 
 server_scripts { 
   -- '@ox_core/imports/server.lua', --Remove the two -- if you're using ox_core in your server
+
   'server/groups/**', 
   'server/main.lua'
 }
-
 
 files { 'client/**/*.lua', 'locales/*.json' }
