b50379b8cd6321d0fc91c3e4a9c5740965e89042
Renčikas <rencikas.business@gmail.com>
2024-07-12 11:40:40 +0300

    fix: core_dispatch alert export

--- server/dispatch/core_dispatch.lua
+++ server/dispatch/core_dispatch.lua
@@ -1,7 +1,8 @@
 Dispatch = function(data)
     for _, job in pairs(data.police_jobs) do
-        TriggerEvent("core_dispatch:addMessage", 
-            data.dispatch_code.." | "..data.message,
+        exports[Config.DispatchResource]:addCall(
+            data.dispatch_code,
+            data.message,
             {},
             { data.pos.x, data.pos.y, data.pos.z },
             job,
@@ -9,7 +10,7 @@ Dispatch = function(data)
             data.sprite,
             data.color,
             true
-        ) 
+        )
     end
 end 
 
