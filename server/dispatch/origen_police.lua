28818f081c6c014dfab91a13b6916858873ad08c
Renčikas <rencikas.business@gmail.com>
2024-07-31 15:25:41 +0300

    fix: origen_dispatch export and event structure refactoring

--- server/dispatch/origen_police.lua
+++ server/dispatch/origen_police.lua
@@ -1,10 +1,14 @@
 Dispatch = function(data)
-    exports["origen_police"]:SendAlert({
-        coords = data.pos.xyz, -- Coordinates vector3(x, y, z) in which the alert is triggered
-        title = data.dispatch_code.." - "..data.message, -- Title in the alert header
-        type = "GENERAL", -- Alert type (GENERAL, RADARS, 215, DRUGS, FORCE, 48X) This is to filter the alerts in the dashboard
-        message = data.message, -- Alert message
-    })
+    for _, job in pairs(data.police_jobs) do
+        exports[Config.DispatchResource]:SendAlert({
+            coords = data.pos.xyz,
+            title = data.dispatch_code,
+            type = 'GENERAL',
+            message = data.message,
+            job = job,
+            metadata = {}
+        })
+    end
 end 
 
 return Dispatch
