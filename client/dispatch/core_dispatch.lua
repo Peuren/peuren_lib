50eb20b84b36fedb6c5a0fc7a73c8f35ac3ca82f
Renčikas <rencikas.business@gmail.com>
2025-07-29 18:00:08 +0300

    Add: codem dispatch

--- client/dispatch/core_dispatch.lua
+++ client/dispatch/core_dispatch.lua
@@ -2,6 +2,6 @@ Dispatch = function(data)
     for _, job in pairs(data.police_jobs) do
         exports[Config.DispatchResource]:addCall(data.dispatch_code, data.message, {}, data.pos.xyz, job, data.time * 1000, data.sprite, data.color)
     end
-end 
+end
 
 return Dispatch
