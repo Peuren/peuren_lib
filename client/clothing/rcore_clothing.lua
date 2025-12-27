8d5c7f7c7527a60eafec4f89175bf4efb28f52cd
Kristijonas Bukavickas <rencikas.business@gmail.com>
2025-12-27 13:15:52 +0200

    Update: rcore_clothing integration

--- client/clothing/rcore_clothing.lua
+++ client/clothing/rcore_clothing.lua
@@ -13,16 +13,21 @@ Clothing = {
 
         Wait(100)
 
-        local undershirt = {component_id = 8, texture = outfit.tshirt_2, drawable = outfit.tshirt_1}
-        local arms = {component_id = 3, texture = 0, drawable = outfit.arms}
-        local torso = {component_id = 11, texture = outfit.torso_2, drawable = outfit.torso_1}
-        local pants = {component_id = 4, texture = outfit.pants_2, drawable = outfit.pants_1}
-        local shoes = {component_id = 6, texture = outfit.shoes_2, drawable = outfit.shoes_1}
-        local accesories = {component_id = 7, texture = outfit.chain_2, drawable = outfit.chain_1}
-        local decals = {component_id = 10, texture = outfit.decals_2, drawable = outfit.decals_1}
-        local props = {props = {{prop_id = 1, texture = outfit.glasses_2, drawable = outfit.glasses_1}, {prop_id = 2, texture = outfit.ears_2, drawable = outfit.ears_1}}}
-
-        exports[Config.ClothingResource]:setPlayerSkin({torso, undershirt, pants, shoes, accesories, arms, decals, props}, false)
+        TriggerEvent('skinchanger:loadSkin', {
+            sex       = GetEntityModel(ped) == `mp_m_freemode_01` and 0 or 1,
+            tshirt_1  = outfit.tshirt_1,
+            tshirt_2  = outfit.tshirt_2,
+            arms      = outfit.arms,
+            arms_2    = 0,
+            torso_1   = outfit.torso_1,
+            torso_2   = outfit.torso_2,
+            pants_1   = outfit.pants_1,
+            pants_2   = outfit.pants_2,
+            shoes_1   = outfit.shoes_1,
+            glasses_1 = outfit.glasses_1,
+            decals_1  = outfit.decals_1,
+            decals_2  = outfit.decals_2,
+        })
     end,
 
     Reset = function()
